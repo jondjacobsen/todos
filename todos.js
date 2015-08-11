7c3b29efee291f3eca0d6bd80fe17ca2c50e552b
jondjacobsen <jonathan.jacobsen@gmail.com>
2015-08-11 06:53:54 -0400

    User Accounts

--- todos.js
+++ todos.js
@@ -77,12 +77,14 @@ if (Meteor.isClient) {
   
   Template.addList.events({
     'submit form': function(event){
-      event.preventDefault();
-      var listName = $('[name=listName]').val();
-      Lists.insert({
+        event.preventDefault();
+        var listName = $('[name=listName]').val();
+        Lists.insert({
           name: listName
-      });
-      $('[name=listName]').val('');
+        }, function(error, results){
+            Router.go('listPage', { _id: results });
+        });
+        $('[name=listName]').val('');
     }
 });
 
