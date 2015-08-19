9be1b798d67902e26c6acdf914bbe5f871d39a09
jondjacobsen <jonathan.jacobsen@gmail.com>
2015-08-18 22:39:28 -0400

    Validation, Day Duex

--- todos.js
+++ todos.js
@@ -102,6 +102,7 @@ if (Meteor.isClient) {
   //Registration form code
   Template.register.events({
     'submit form': function(event){
+      /*
       event.preventDefault();
       var email = $('[name=email]').val();
       var password = $('[name=password').val();
@@ -115,6 +116,7 @@ if (Meteor.isClient) {
           Router.go("home");
         }
       });
+      */
   }
 });
 
@@ -144,8 +146,38 @@ if (Meteor.isClient) {
         }
       });
       */
+      }
+    });
+/* Validation of Forms - GENERAL RULES */
+
+$.validator.setDefaults({
+  rules: {
+    email: {
+      required: true,
+      email: true
+    },
+    password: {
+      required: true,
+      minlength: 6
     }
-  });
+  },
+  messages: {
+        email: {
+            required: "You must enter an email address.",
+            email: "You've entered an invalid email address."
+        },
+        password: {
+            required: "You must enter a password.",
+            minlength: "Your password must be at least {0} characters."
+        }
+    }
+});
+
+
+      Template.register.onRendered(function(){
+    $('.register').validate();
+    });
+
   /* VALIDATION Demonstration */
 
   Template.login.onCreated(function(){
