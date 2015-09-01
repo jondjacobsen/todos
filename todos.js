e282227a5c42b45e91db9ebaa4434f5008c1ac65
jondjacobsen <jonathan.jacobsen@gmail.com>
2015-08-31 23:32:48 -0400

    List Item Changes

--- todos.js
+++ todos.js
@@ -263,6 +263,10 @@ if (Meteor.isServer) {
       if(!currentUser){
         throw new Meteor.Error("not-logged-in", "You're not logged-in.");
       }
+      var currentList = Lists.findOne(currentList);
+      if(currentList.createdBy != currentUser){
+        throw new Meteor.Error("invalid-user", "You don't own that list.");
+      }
       return Todos.insert(data);
     }
   });
